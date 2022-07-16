2a6d24aeffbdb25328e06fbf27a04bfac3ef4d84
Kal <info@kalacademy.org>
2022-07-16 11:02:37 -0700

    Product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
 import {Container} from 'react-bootstrap'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
     return (
     <>
-      <Header />
-      <main>
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
+      <Router>
+        <Header />
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" exact element={<HomeScreen />} />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 }
