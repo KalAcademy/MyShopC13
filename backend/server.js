af7406974a666dab8bcddc9894ee8cd1eea95e6b
Kal <info@kalacademy.org>
2022-08-28 10:50:55 -0700

    Prepared to deploy

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,11 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import errorHandler from './middleware/errorMiddleware.js'
+import path from 'path'
 
 const app = express()
 dotenv.config()
 connectDB()
-
-
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
 
 app.use(express.json())
 app.use('/api/products', productRoutes)
@@ -22,6 +18,20 @@ app.use('/api/orders', orderRoutes)
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 app.use(errorHandler)
 
 const port = process.env.PORT || 5000
